f291f317085040ec5eea9fdf855d34ffdfa6488e
Patrick Vacca <patrickj.vacca@gmail.com>
2020-06-09 23:09:08 -0400

    version1

--- jeopardy/jeopardy.js
+++ jeopardy/jeopardy.js
@@ -20,7 +20,7 @@
 
 let categories = [];
 let categoryIds = [];
-
+let startCounter = 0;
 
 /** Get NUM_CATEGORIES random category from API.
  *
@@ -31,11 +31,10 @@ async function getCategoryIds() {
     const response = await axios.get(`http://jservice.io/api/categories`,{params:{count: 100}});
     for (i=0;i<6;i++){
         let index = Math.floor(Math.random()*100)
+        //ATTENTION NEEDED add a way to ensure that each id is unique
         categoryIds.push(response.data[index])
     }
 } 
-
-getCategoryIds();
 
 /** Return object with data about a category:
  *
@@ -62,20 +61,11 @@ async function getCategory(catId) {
 }
 
 async function buildCategories(ids){
-    console.log("HELLO");
     for (id of ids){
         let response = await getCategory(id.id);
        categories.push(response)
     }
-    console.log(categories);
 }
-
-
-buildCategories(categoryIds);
-
-
-
-
 
 /** Fill the HTML table#jeopardy with the categories & cells for questions.
  *
@@ -93,43 +83,85 @@ async function fillTable() {
     for (let x=0; x<6; x++){
         const headCell = document.createElement("td");
         headCell.innerText = categories[x].title
-        headCell.setAttribute("id",x);
+        //86headCell.setAttribute("id",x);
         top.append(headCell)
     }
     jeopardyTable.append(top);
+    
     for (let y = 0;y<5;y++){
         const row = document.createElement("tr");
         for (let x = 0; x<6 ;x++){
             const cell = document.createElement("td");
-            cell.setAttribute("id",`${y}-${x}`);
-            cell.classList.add("question")
-            cell.innerText = "?"
+            cell.setAttribute("id",`${x}-${y}`);
+            //86cell.classList.add("question");
+            cell.addEventListener("click",handleClick);
+
+            cell.innerHTML = '<i class="fas fa-question-circle"></i>';
             //cell.innerText = categories[x].clues[y].question
             row.append(cell);
         }
+        //86row.setAttribute("question",`${y}`)
         jeopardyTable.append(row);
     }
 }
 
-
-
 function handleClick(evt) {
+
+    let cellUse = evt.target.closest("td")
+    let x = (cellUse.id).substring(0,1)
+    let y = (cellUse.id).substring(2,3)
+    let question = $(`#${x}-${y}`)
+    if(question.hasClass('showing')){
+        question.text(categories[x].clues[y].answer)
+        question.addClass('answered')
+    } else {
+        question.text(categories[x].clues[y].question)
+        question.addClass('showing');
+    }
+    //86console.log(question.hasClass('showing'))
+    //86console.log(`${x}-${y}`);
+    //86question.text(categories[x].clues[y].question)
 }
-$(".question").on("click",function(){
-    alert("HELLO");
-});
+
+/*86
+function handleClick(evt) {
+    console.log(evt.target)
+    let x = (evt.target.id).substring(0,1)
+    let y = (evt.target.id).substring(2,3)
+    let question = $(`#${x}-${y}`)
+    console.log(`${x}-${y}`);
+    question.text(categories[x].clues[y].question)
+}86*/
 
 /** Wipe the current Jeopardy board, show the loading spinner,
  * and update the button used to fetch data.
  */
 
-function showLoadingView() {
+function showLoad() {
+    let gameSpace = document.getElementById("game");
+    gameSpace.innerHTML = '<i class="fa fa-spinner fa-spin fa-6x text-primary" aria-hidden-"true"></i>'
+    $("button").eq(0).text("Loading...")
+    
+    getCategoryIds();
 
+    setTimeout(function(){
+        buildCategories(categoryIds);
+        $("button").eq(0).text("Restart!")
+        gameSpace.innerHTML='<div id="jeopardy" class="container"></div>'
+    },1500)
+    setTimeout(function(){
+        fillTable();
+    },2000);
 }
 
 /** Remove the loading spinner and update the button used to fetch data. */
 
-function hideLoadingView() {
+function restartPage() {
+    $("button").eq(0).text("Start!")
+    let gameSpace = document.getElementById("game");
+    gameSpace.innerHTML = '';
+    categoryIds = [];
+    categories = []
 }
 
 /** Start game:
@@ -139,11 +171,14 @@ function hideLoadingView() {
  * - create HTML table
  * */
 
-async function setupAndStart() {
-}
+//86async function setupAndStart() {
+//86}
 
 /** On click of start / restart button, set up game. */
-
+$("#start").on("click",function(){
+    startCounter === 0 ? showLoad():restartPage();
+    startCounter ===0 ? startCounter = 1 : startCounter = 0;
+});
 // TODO
 
 /** On page load, add event handler for clicking clues */
